6df20bab2a933617f99458e897c3a32b4133b197
Miftari Simel <miftarisimel@gmail.com>
2025-05-28 17:41:07 +0200

    updates to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-import ChooseUs from "@/components/Home/ChooseUs";
-import Contact from "@/components/Home/Contact";
-import Hero from "@/components/Home/Hero";
-import Projects from "@/components/Home/Projects";
-import Services from "@/components/Home/Services";
 
 export default function Home() {
 	return (
 		<div className="">
-			<Hero />
+			{/* <Hero />
 			<Services />
 			<ChooseUs />
 			<Projects />
-			<Contact />
+			<Contact /> */}
 		</div>
 	);
 }
